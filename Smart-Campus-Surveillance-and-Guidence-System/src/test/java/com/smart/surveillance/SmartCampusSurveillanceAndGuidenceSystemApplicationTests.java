c6e3228b1acb84306583c93fcb9176a7b96476d9
AweshAlam <aweshalam53@gmail.com>
2024-09-21 22:09:22 +0530

    Upload

---
+++ Smart-Campus-Surveillance-and-Guidence-System/src/test/java/com/smart/surveillance/SmartCampusSurveillanceAndGuidenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.surveillance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusSurveillanceAndGuidenceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
